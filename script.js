35e35c7ca9d602cde55eae6a9c8a627d1f66d619
Jennifer Naasz <naasz.jl@gmail.com>
2017-04-26 09:39:17 -0400

    .

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
-    
+
     var name = $('#firstName' + '#lastName'),
         email = $('email'),
         phone = $('#phoneNum'),
-        seat = $('numSeat');
+        seat = $('numSeat'),;
+
 
 
     var userInfo = {
         name: 'Name',
-        seat: 'B2',
+        seat: 'B2', 'B3',
         phone: 5555555555,
         email: 'email@site.com'
+
     };
 
 
 });
+
+
+userInfo.name
