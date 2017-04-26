1656e3fb4f3244cdbe12357233afae0e97a84dce
Jennifer Naasz <naasz.jl@gmail.com>
2017-04-26 13:05:50 -0400

    test

--- script.js
+++ script.js
@@ -9,19 +9,59 @@ $(document).ready(function() {
 
     var userInfo = {
         name: 'Name',
-        seat: 'B2',
+        seat: '',
         phone: 5555555555,
         email: 'email@site.com'
 
     };
 
+
+
 //------------------------------------------------
 
-    $('.row div').on("click", function(){
-      $(this).css('background-color', 'red');
-      userInfo.seat = 
 
-    })
+$('.row div').on('click', function(){
+  $(this).toggleClass('red');
+  if()
+
+  $(this).css('background-color', 'red');
+  var seatNumber = this.innerHTML;
+  userInfo.seat += seatNumber;
+  console.log(userInfo.seat);
+});
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").toggleClass("main");
+    });
+});
+</script>
+<style>
+.main {
+    font-size: 120%;
+    color: red;
+}
+
+
+//$('.row div').off('click');
+
+// $('.row div').on('click',function() {
+//   $(this).toggle(
+//   function(){
+//     $('.row div').css('background-color', 'red');
+//     var seatNumber = this.innerHTML;
+//     userInfo.seat += seatNumber;
+//     console.log(userInfo.seat);
+//   }, function() {
+//     $(this).css('background-color', 'green');
+//     var seatNumber = this.innerHTML;
+//     userInfo.seat -= seatNumber;
+//     console.log(userInfo.seat);
+// });
+// });
+
+
 
 
 //  $("#A1").mouseenter(function() {
