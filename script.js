f38dc9e7343c380b11952323727e91c23e1a9c8c
Steven Turner <Stevent70899@gmail.com>
2017-04-27 09:15:25 -0400

    front

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var name = $('#firstName' + '#lastName'),
         email = $('email'),
         phone = $('#phoneNum'),
-        seat = $('numSeat'),;
+        seat = $('numSeat');
 
 
 
@@ -15,10 +15,23 @@ $(document).ready(function() {
 
     };
 
-//------------------------------------------------
+    var slideIndex = 1;
+showDivs(slideIndex);
 
-    $()
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
 
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
 
 });
 
